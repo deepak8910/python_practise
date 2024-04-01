98e84894b0e14b0de1926740566dd01a05e9391f
deepak8910 <deepak.pulgurle@gmail.com>
2024-04-01 18:41:21 +0530

    Add files via upload

--- kahns.py
+++ kahns.py
@@ -9,6 +9,14 @@ def get_depth(n,testsets):
 
 def get_seq(n,testsets):
     print(testsets)
+    sets = [set(i) for i in testsets]
+    list1 = []
+    for i in sets:
+        if i not in list1:
+            list1.append(i)
+    if len(list1) != len(sets):
+        return("deadlock detected")
+    
     depth = get_depth(n,testsets)
     answer = [x for x in depth.keys() if depth[x] == 0]
     while  len(testsets) > 0:
@@ -26,4 +34,4 @@ def get_seq(n,testsets):
 # Here 2 is dependend on 1, 3 depends on 2, 5 depends on 1 and so on        
 print(get_seq(10,[(1,2),(2,3),(1,5),(2,3),(7,6),(9,2),(8,0)]))
 # here 2 depends on 3, 3 depends on 1, 4 depends on 0, 4 also depends on 1 and so on
-print(get_seq(6,[(3,2),(1,3),(0,4),(1,4),(0,5),(2,5)]))
+print(get_seq(6,[(2,3),(3,2),(1,3),(0,4),(1,4),(0,5),(2,5)]))
